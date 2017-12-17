b5ff012e61920ec8e08427ae86b92518411598fa
Russell Dobda <russ.dobda@synervoz.com>
2017-12-17 12:18:55 -0700

    adding hello world

---
+++ Vipassana/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vipassana
+//
+//  Created by Mr Russell on 12/17/17.
+//  Copyright © 2017 Russell Eric Dobda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
